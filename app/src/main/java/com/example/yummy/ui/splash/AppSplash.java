45bf72bfee49fc9e1dbacdec157fedf4edc765ad
MahmoudAladwy <mahmoudaladwy774@gmail.com>
2026-02-01 00:36:32 +0200

    - update splash logic view

--- app/src/main/java/com/example/yummy/ui/splash/AppSplash.java
+++ app/src/main/java/com/example/yummy/ui/splash/AppSplash.java
@@ -3,6 +3,7 @@ package com.example.yummy.ui.splash;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,8 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.example.yummy.R;
-import com.example.yummy.ui.auth.AuthActivity;
 import com.example.yummy.ui.home.HomeActivity;
+import com.example.yummy.ui.onboarding.OnBoarding;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class AppSplash extends AppCompatActivity {
@@ -22,17 +23,30 @@ public class AppSplash extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_app_splash);
-        LottieAnimationView lottie = findViewById(R.id.lottieView);
 
+        LottieAnimationView lottie = findViewById(R.id.lottieView);
         TextView appName = findViewById(R.id.appNameText);
+
         appName.animate()
                 .alpha(1f)
                 .setDuration(1900)
                 .setStartDelay(500)
                 .start();
-        
 
         FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        SharedPreferences prefs =
+                getSharedPreferences("yummy_prefs", MODE_PRIVATE);
+
+        boolean isFirstRun = prefs.getBoolean("FIRST_RUN", true);
+
+        if (isFirstRun) {
+            auth.signOut();
+            prefs.edit()
+                    .putBoolean("FIRST_RUN", false)
+                    .apply();
+        }
+
         if (auth.getCurrentUser() != null) {
             lottie.addAnimatorListener(new AnimatorListenerAdapter() {
                 @Override
@@ -41,16 +55,14 @@ public class AppSplash extends AppCompatActivity {
                     finish();
                 }
             });
-
         } else {
             lottie.addAnimatorListener(new AnimatorListenerAdapter() {
                 @Override
                 public void onAnimationEnd(Animator animation) {
-                    startActivity(new Intent(AppSplash.this, AuthActivity.class));
+                    startActivity(new Intent(AppSplash.this, OnBoarding.class));
                     finish();
                 }
             });
         }
-
     }
 }
